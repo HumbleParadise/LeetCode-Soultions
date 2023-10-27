46e3a83b67cfb820db50d635d46ce7ec6b33417e
HumbleParadise <143842466+HumbleParadise@users.noreply.github.com>
2023-10-27 14:00:31 +0530

    Time: 78 ms (20.08%) | Memory: 10.7 MB (28.35%) - LeetSync

---
+++ sliding-window/Medium/843-binary-trees-with-factors/binary-trees-with-factors.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        set<int> s;
+        int n = arr.size();
+        int cnt = 0;
+        map<int, long long> Mp;
+        sort(arr.begin(), arr.end());
+        int ans = 0;
+        
+        int mod = 1e9 + 7;
+        
+        for(int i=0;i<n;i++)
+        {
+            s.insert(arr[i]);
+            Mp[arr[i]]++;
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                int root = arr[i];
+                int left = arr[j];
+               if(root%arr[j] == 0 && arr[j] < root)
+               {
+                   int right = root/left;
+                   if(s.count(right) > 0)
+                   {
+                       
+                       // int left = arr[j];
+                       // int right = ele;
+                       
+                       Mp[root] += (Mp[left] * Mp[right]);
+                   }
+               }
+                  
+            }
+        }
+        
+        for(auto ite: Mp) ans = (ans + ite.second)%mod;
+        
+        return ans;
+        
+    }
+};
